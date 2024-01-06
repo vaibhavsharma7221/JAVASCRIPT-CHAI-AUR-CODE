93754f6eaadceb761daaad23d0a372e9ba5adaa3
Vaibhav_7221 <76476010+vaibhavsharma7221@users.noreply.github.com>
2024-01-06 20:12:25 +0530

    Create 1) Let,const,var ki khani.js

---
+++ 1) Let,const,var ki khani.js
@@ -0,0 +1,27 @@
+const accountId=144553 // value wont change
+
+let accountEmail="new@gmail.com"
+
+var accountPassword="12345";//old method
+
+accountCity="himachal";
+
+let accountState;
+
+//accountId=2;// const variable value don't change
+
+accountEmail="kanchi@7221";
+accountPassword="6531"// semi-colon is optionl
+accountCity="banglore"
+
+console.log(accountId);
+
+/*
+prefer not to use var because var has block issues and
+let don't have any block issues
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountState])
+
+
+
